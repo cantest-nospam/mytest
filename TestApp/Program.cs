1efc7f2fcfadfb9b0e0525f4e9b4c85fbb28b173
cantest-nospam <103596984+cantest-nospam@users.noreply.github.com>
2022-06-01 14:20:53 -0400

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("==================================================");
 Console.WriteLine("This is a test");
+Console.WriteLine(Environment.GetEnvironmentVariable("TEMP_SECRET"));
